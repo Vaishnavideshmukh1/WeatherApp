71a92f205ff80a41a6cc4f576bacffb4a4c8c7b0
Vaishnavi Deshmukh <vaishnavi@example.com>
2024-09-03 19:57:06 +0530

    Initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,6 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+    key: "70f1617b399cdbb40854d87d7bbc80fe",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
+  
